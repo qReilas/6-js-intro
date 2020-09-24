4e72e7095d460cfc8b0ecaa86a727a16888bbbff
qReilas <Edvardas.nugaris@gmail.com>
2020-09-24 10:13:33 +0300

    Initial commit

---
+++ js/loops.js
@@ -0,0 +1,7 @@
+const seasons = ['pavasaris', 'vasara', 'ruduo', 'ziema'];
+const seasonscount = seasons.length;
+console.log(seasons);
+
+for (let i=0; i<seasonsCount; i++) {
+        console.log(seasons[i]);
+}
